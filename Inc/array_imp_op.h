2cbdad292fd4906d7f119ee43a51d138b0d0febd
sujith-2006 <150604521+sujith-2006@users.noreply.github.com>
2025-08-16 20:36:00 +0530

    Function Declarations of Array Implementation

---
+++ Inc/array_imp_op.h
@@ -0,0 +1,6 @@
+#define MAX 100
+void defineArray(int arr[], int *n);
+void printArray(int arr[], int *n);
+void insertArray(int arr[], int *n);
+void deleteArray(int arr[], int *n);
+void updateArray(int arr[], int *n);
